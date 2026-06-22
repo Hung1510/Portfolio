1fba3622cc754df0214b87b3ba7e4a2e134cdcd5
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2026-06-23 04:21:04 +0700

    update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
-import SmartAdvisorDetail from "./pages/projectPage/SmartAdvisorDetail";
 import { ProjectBlog } from "./pages/ProjectBlog";
+import { projects } from "./components/ProjectsSection";
+import { detailPages } from "./pages/projectPage";
 // import { ThemeToggle } from "./components/ThemeToggle";
 
 
@@ -15,10 +16,25 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          {/* Smart Learning keeps its dedicated detail page */}
-          <Route path="/projects/smart-learning-advisor" element={<SmartAdvisorDetail />} />
-          {/* All other projects use the generic blog page, keyed by numeric id */}
-          <Route path="/projects/:id" element={<ProjectBlog />} />
+
+          {/* Project detail routes are generated from the projects array.
+              A project with a slug listed in detailPages gets its dedicated
+              page; every other slugged project gets the generic ProjectBlog. */}
+          {projects
+            .filter((project) => project.slug)
+            .map((project) => {
+              const CustomPage = detailPages[project.slug];
+              return (
+                <Route
+                  key={project.slug}
+                  path={`/projects/${project.slug}`}
+                  element={
+                    CustomPage ? <CustomPage /> : <ProjectBlog slug={project.slug} />
+                  }
+                />
+              );
+            })}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
